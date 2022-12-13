9be7cc09d8959a97deef3224496bed59bbc9bb08
SanjuCse <[200~sanjaya1792000@gmail.com>
2022-12-13 13:12:47 +0530

    modified and added MAC hyphen replace program

---
+++ com.core.java/src/com/core/string/PlaceHyphen.java
@@ -0,0 +1,22 @@
+package com.core.string;
+
+import java.util.Scanner;
+
+public class PlaceHyphen {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		String MAC = (String) scanner.nextLine();
+		
+		System.out.println(MAC.hashCode());
+		MAC = new StringBuffer(MAC)
+				.insert(MAC.length()-2, "-")
+				.insert(MAC.length()-4, "-")
+				.insert(MAC.length()-6, "-")
+				.insert(MAC.length()-8, "-")
+				.insert(MAC.length()-10, "-")
+				.toString();
+		System.out.println(MAC);
+		System.out.println(MAC.hashCode());
+		scanner.close();
+	}
+}
